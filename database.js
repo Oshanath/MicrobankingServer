3bac0300d1f87add796bf0cdf668f642c5d0d482
LasithRajakarunaratn <lasith.19@cse.mrt.ac.lk>
2022-06-11 11:18:23 +0530

    Split database code to a seperate file

---
+++ database.js
@@ -0,0 +1,72 @@
+const mysql = require('mysql');
+let database = null;
+
+function createConnection(){
+    database = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'mydb'
+    });
+    return database;
+}
+
+function dropTablesAndInsertDummyData(){
+    database.query("DROP TABLE account_customer");
+    database.query("DROP TABLE account");
+    database.query("DROP TABLE customer");
+    database.query("DROP TABLE agent");
+    database.query("DROP TABLE account_critical");
+
+    database.query("CREATE TABLE account(number INT, balance FLOAT NOT NULL, joint BOOLEAN NOT NULL, registered BOOLEAN NOT NULL, PRIMARY KEY (number));");
+    database.query("CREATE TABLE agent(agentID VARCHAR(20), name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, PRIMARY KEY (agentID));");
+    database.query("CREATE TABLE customer(nic VARCHAR(20), name VARCHAR(50) NOT NULL, agentID VARCHAR(20) NOT NULL, PRIMARY KEY (nic), FOREIGN KEY (agentID) references agent(agentID));");
+    database.query("CREATE TABLE account_customer(number INT, nic VARCHAR(20), FOREIGN KEY (number) REFERENCES account(number), FOREIGN KEY (nic) REFERENCES customer(nic));");
+    database.query("CREATE TABLE account_critical(number INT, critical BOOLEAN);");
+
+    database.query("INSERT INTO account VALUES (12332555, 45666.56, true, true);");
+    database.query("INSERT INTO account VALUES (65468467, 45666.56, true, false);");
+    database.query("INSERT INTO account VALUES (34635764, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (33455546, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (85469699, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (45673858, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (09887755, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (16683568, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (23580987, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (10885446, 45666.56, false, true);");
+    database.query("INSERT INTO account VALUES (65584445, 45666.56, false, false);");
+    database.query("INSERT INTO account VALUES (78654555, 45666.56, false, false);");
+
+    database.query("INSERT INTO agent VALUES(\"190488J\", \"Oshanath\", \"password\");");
+    database.query("INSERT INTO agent VALUES(\"190564L\", \"Rajawasam\", \"password\");");
+
+    database.query("INSERT INTO customer VALUES(\"991741135v\", \"Lasith\", \"190488J\");");
+    database.query("INSERT INTO customer VALUES(\"246757377f\", \"Ravindu\", \"190488J\");");
+    database.query("INSERT INTO customer VALUES(\"34634g575jj\", \"Thilina\", \"190564L\");");
+    database.query("INSERT INTO customer VALUES(\"rsgrrd44646\", \"Madaya\", \"190564L\");");
+
+    database.query("INSERT INTO account_customer VALUES(12332555, \"991741135v\");");
+    database.query("INSERT INTO account_customer VALUES(65468467, \"991741135v\");");
+    database.query("INSERT INTO account_customer VALUES(34635764, \"991741135v\");");
+    database.query("INSERT INTO account_customer VALUES(33455546, \"246757377f\");");
+    database.query("INSERT INTO account_customer VALUES(45673858, \"246757377f\");");
+    database.query("INSERT INTO account_customer VALUES(09887755, \"34634g575jj\");");
+    database.query("INSERT INTO account_customer VALUES(09887755, \"rsgrrd44646\");");
+
+    database.query("INSERT INTO account_critical VALUES(12332555, true);");
+    database.query("INSERT INTO account_critical VALUES(34635764, false);");
+    database.query("INSERT INTO account_critical VALUES(33455546, false);");
+    database.query("INSERT INTO account_critical VALUES(85469699, true);");
+    database.query("INSERT INTO account_critical VALUES(45673858, false);");
+    database.query("INSERT INTO account_critical VALUES(09887755, false);");
+    database.query("INSERT INTO account_critical VALUES(16683568, false);");
+    database.query("INSERT INTO account_critical VALUES(23580987, true);");
+    database.query("INSERT INTO account_critical VALUES(10885446, false);");
+}
+
+
+
+module.exports = {
+    createConnection,
+    dropTablesAndInsertDummyData
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const mysql = require('mysql');
+const db = require("./database")
 
 var app = express();
 
@@ -31,69 +31,12 @@ var server = app.listen(8081, function () {
    console.log("Example app listening at http://", host, port);
 });
 
-// MySQL server
-const database = mysql.createConnection({
-   host: 'localhost',
-   user: 'root',
-   password: 'password',
-   database: 'mydb'
- });
+const database = db.createConnection();
 
 database.connect((err) => {
    if (err) throw err;
    console.log('Connected to MySQL Server!');
 
-   database.query("DROP TABLE account_customer");
-   database.query("DROP TABLE account");
-   database.query("DROP TABLE customer");
-   database.query("DROP TABLE agent");
-   database.query("DROP TABLE account_critical");
-
-   database.query("CREATE TABLE account(number INT, balance FLOAT NOT NULL, joint BOOLEAN NOT NULL, registered BOOLEAN NOT NULL, PRIMARY KEY (number));");
-   database.query("CREATE TABLE agent(agentID VARCHAR(20), name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, PRIMARY KEY (agentID));");
-   database.query("CREATE TABLE customer(nic VARCHAR(20), name VARCHAR(50) NOT NULL, agentID VARCHAR(20) NOT NULL, PRIMARY KEY (nic), FOREIGN KEY (agentID) references agent(agentID));");
-   database.query("CREATE TABLE account_customer(number INT, nic VARCHAR(20), FOREIGN KEY (number) REFERENCES account(number), FOREIGN KEY (nic) REFERENCES customer(nic));");
-   database.query("CREATE TABLE account_critical(number INT, critical BOOLEAN);");
-
-   database.query("INSERT INTO account VALUES (12332555, 45666.56, true, true);");
-   database.query("INSERT INTO account VALUES (65468467, 45666.56, true, false);");
-   database.query("INSERT INTO account VALUES (34635764, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (33455546, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (85469699, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (45673858, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (09887755, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (16683568, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (23580987, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (10885446, 45666.56, false, true);");
-   database.query("INSERT INTO account VALUES (65584445, 45666.56, false, false);");
-   database.query("INSERT INTO account VALUES (78654555, 45666.56, false, false);");
-
-   database.query("INSERT INTO agent VALUES(\"190488J\", \"Oshanath\", \"password\");");
-   database.query("INSERT INTO agent VALUES(\"190564L\", \"Rajawasam\", \"password\");");
-
-   database.query("INSERT INTO customer VALUES(\"991741135v\", \"Lasith\", \"190488J\");");
-   database.query("INSERT INTO customer VALUES(\"246757377f\", \"Ravindu\", \"190488J\");");
-   database.query("INSERT INTO customer VALUES(\"34634g575jj\", \"Thilina\", \"190564L\");");
-   database.query("INSERT INTO customer VALUES(\"rsgrrd44646\", \"Madaya\", \"190564L\");");
-
-   database.query("INSERT INTO account_customer VALUES(12332555, \"991741135v\");");
-   database.query("INSERT INTO account_customer VALUES(65468467, \"991741135v\");");
-   database.query("INSERT INTO account_customer VALUES(34635764, \"991741135v\");");
-   database.query("INSERT INTO account_customer VALUES(33455546, \"246757377f\");");
-   database.query("INSERT INTO account_customer VALUES(45673858, \"246757377f\");");
-   database.query("INSERT INTO account_customer VALUES(09887755, \"34634g575jj\");");
-   database.query("INSERT INTO account_customer VALUES(09887755, \"rsgrrd44646\");");
-
-   database.query("INSERT INTO account_critical VALUES(12332555, true);");
-   database.query("INSERT INTO account_critical VALUES(34635764, false);");
-   database.query("INSERT INTO account_critical VALUES(33455546, false);");
-   database.query("INSERT INTO account_critical VALUES(85469699, true);");
-   database.query("INSERT INTO account_critical VALUES(45673858, false);");
-   database.query("INSERT INTO account_critical VALUES(09887755, false);");
-   database.query("INSERT INTO account_critical VALUES(16683568, false);");
-   database.query("INSERT INTO account_critical VALUES(23580987, true);");
-   database.query("INSERT INTO account_critical VALUES(10885446, false);");
+   db.dropTablesAndInsertDummyData();
 
 });
-
-console.log("here");
